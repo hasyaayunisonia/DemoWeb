8b09053604dce680f0ab1282664b7335e965e7c2
hasya <hasya.ayuni.tif20@polban.ac.id>
2025-07-03 11:57:17 +0700

    add new library and mock

---
+++ src/api/mock.ts
@@ -0,0 +1,43 @@
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import type { AxiosProgressEvent } from "axios";
+
+const mock = new AxiosMockAdapter(axios, { delayResponse: 500 });
+
+// Mock Upload API
+mock.onPost("/api/upload").reply(async (config) => {
+  return new Promise((resolve) => {
+    let loaded = 0;
+    const total = 1000000;
+    const interval = setInterval(() => {
+      loaded += 100000;
+      const fakeProgress = {
+        lengthComputable: true,
+        loaded,
+        total,
+      } as AxiosProgressEvent;
+
+      if (config.onUploadProgress) {
+        config.onUploadProgress(fakeProgress);
+      }
+
+      if (loaded >= total) {
+        clearInterval(interval);
+        resolve([200, { status: true, message: "Upload sukses" }]);
+      }
+    }, 300);
+  });
+});
+
+// Mock Submit Form
+mock.onPost("/api/submit-form").reply(async (config) => {
+  const delay = 800;
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      console.log("Mocked received FormData:", config.data);
+      resolve([200, { status: true, message: "Form berhasil diterima" }]);
+    }, delay)
+  );
+});
+
+export default mock;
